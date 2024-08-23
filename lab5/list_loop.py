4fe85ea781c0e8d4e434c275971ecfa12ebd392b
nandinisamdariya <nandini.samdariya10@gmail.com>
2024-08-23 10:12:32 +0530

    Python Program

--- lab5/list_loop.py
+++ lab5/list_loop.py
@@ -1,5 +1,4 @@
 tech_list = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
 
-# Iterating through the list and printing items
 for tech in tech_list:
     print(tech)
--- lab5/set.py
+++ lab5/set.py
@@ -4,15 +4,19 @@ B = {19, 22, 20, 25, 26, 24, 28, 27}
 
 # I. Find the length of the set it_companies
 len_it_companies = len(it_companies)
+print(len_it_companies)
 
 # II. Add 'Twitter' to it_companies
 it_companies.add('Twitter')
+print(it_companies)
 
 # III. Insert multiple IT companies at once to the set it_companies
 it_companies.update({'Spotify', 'LinkedIn', 'Tesla'})
+print(it_companies)
 
 # IV. Remove one of the companies from the set it_companies
 it_companies.discard('Facebook')
+print(it_companies)
 
 # V. Difference between remove and discard: 
 # it_companies.remove('Facebook')
